f3bef1e5fcbbd2282f3c29a5964c0a90a364281a
Andy A <anders.a9010@gmail.com>
2019-01-14 15:53:07 +0700

    Added wsdl output in serviceHotel

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -4,6 +4,7 @@ namespace Devlabs91\GenericOtaHotelApiService\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class ServiceController extends AbstractController {
 
@@ -24,17 +25,29 @@ class ServiceController extends AbstractController {
         return $response;        
     }
 
-    public function serviceHotel()
+    public function serviceHotel( Request $request )
     {
+        if($request->getMethod()=='GET') { 
+            $params = [];parse_str( $request->normalizeQueryString( $request->getQueryString() ), $params);
+            if(!key_exists('wsdl', $params)) {
+                $response = new Response( '', Response::HTTP_BAD_REQUEST, [ 'HTTP/1.1 400 Client Error' ] );
+                return $response;
+            }
+            $content = file_get_contents(__DIR__."/../Schemas/index.wsdl");
+            $response = new Response( $content, Response::HTTP_OK, [ 'Content-Type: application/wsdl+xml' ] );
+            return $response;
+        }
+        if($request->getMethod()!='POST') { 
+            return new Response( '', Response::HTTP_BAD_REQUEST, [ 'HTTP/1.1 400 Client Error' ] );
+        }
         
         $hotelService = $this->get('hotel_service');
-
+        
         $options = array (
             'classmap' => \Devlabs91\GenericOtaHotelApiService\ClassMap::get()
         );
         
         $soapServer = new \SoapServer( __DIR__."/../Schemas/index.wsdl", $options);
-        
         $soapServer->setObject($hotelService);
         
         $response = new Response();
@@ -45,5 +58,5 @@ class ServiceController extends AbstractController {
         $response->setContent(ob_get_clean());
         return $response;
     }
-    
+
 }
